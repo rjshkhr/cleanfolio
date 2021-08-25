8d64223bc8423eca248eeea80c227d3139f1e01f
rajshekhar26 <rjshekhar.official@gmail.com>
2021-08-25 12:51:33 +0530

    Social links won't show up if social object doesn't exist

--- src/components/About/About.js
+++ src/components/About/About.js
@@ -8,10 +8,12 @@ const About = () => {
 
   return (
     <div className='about center'>
-      <h1>
-        Hi, I am <span className='about__name'>{name}.</span>
-      </h1>
-      <h2 className='about__role'>A {role}.</h2>
+      {name && (
+        <h1>
+          Hi, I am <span className='about__name'>{name}.</span>
+        </h1>
+      )}
+      {role && <h2 className='about__role'>A {role}.</h2>}
       <p className='about__desc'>{description && description}</p>
       <div className='about__contact center'>
         {resume && (
@@ -21,23 +23,27 @@ const About = () => {
             </button>
           </a>
         )}
-        {social.github && (
-          <a
-            href={social.github}
-            aria-label='github'
-            className='link link--icon'
-          >
-            <GitHubIcon />
-          </a>
-        )}
-        {social.linkedin && (
-          <a
-            href={social.linkedin}
-            aria-label='linkedin'
-            className='link link--icon'
-          >
-            <LinkedInIcon />
-          </a>
+        {social && (
+          <div>
+            {social.github && (
+              <a
+                href={social.github}
+                aria-label='github'
+                className='link link--icon'
+              >
+                <GitHubIcon />
+              </a>
+            )}
+            {social.linkedin && (
+              <a
+                href={social.linkedin}
+                aria-label='linkedin'
+                className='link link--icon'
+              >
+                <LinkedInIcon />
+              </a>
+            )}
+          </div>
         )}
       </div>
     </div>

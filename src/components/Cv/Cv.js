90e1a0a956112274e5ad777a0e3986eb22923604
Mario Nikhil Pereira <45009203+nmpereira@users.noreply.github.com>
2023-04-28 15:26:45 -0400

    create route to /cv

---
+++ src/components/Cv/Cv.js
@@ -0,0 +1,6 @@
+function Cv() {
+  window.location.assign('/nmpereira_cv.pdf')
+  return null
+}
+
+export default Cv
--- src/index.js
+++ src/index.js
@@ -1,11 +1,18 @@
 import { render } from 'react-dom'
+import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom'
 import App from './App'
 import { ThemeProvider } from './contexts/theme'
 import './index.css'
+import Cv from './components/Cv/Cv'
 
 render(
   <ThemeProvider>
-    <App />
+    <BrowserRouter>
+      <Routes>
+        <Route path='/' element={<App />} />
+        <Route path='/cv' element={<Cv />} />
+      </Routes>
+    </BrowserRouter>
   </ThemeProvider>,
   document.getElementById('root')
 )

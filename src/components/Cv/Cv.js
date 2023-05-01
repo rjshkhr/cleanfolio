6fa2b180d4afe07362f207c22348173bc733c6bc
nmpereira <nikhilpereira25@gmail.com>
2023-05-01 04:03:12 -0400

    Update Cv.js

--- src/components/Cv/Cv.js
+++ src/components/Cv/Cv.js
@@ -1,10 +1,11 @@
 function Cv() {
   console.log('CV page')
   // remove /cv from url
-  const domain = window.location.href.replace(/\/cv$/, '')
-  console.log({ domain })
-  window.location.href = `${domain}/nmpereira_cv.pdf`
-  return null
+  // const domain = window.location.href.replace(/\/cv$/, '')
+  // console.log({ domain })
+  // window.location.href = `${domain}/nmpereira_cv.pdf`
+
+  return <h1>CV</h1>
 }
 
 export default Cv

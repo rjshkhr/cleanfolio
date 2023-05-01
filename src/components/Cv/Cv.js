302c874af9712fce33db5aedced7594469e4a9ce
nmpereira <nikhilpereira25@gmail.com>
2023-05-01 04:15:22 -0400

    update resume and fix cv page

--- src/components/Cv/Cv.js
+++ src/components/Cv/Cv.js
@@ -1,11 +1,8 @@
 function Cv() {
-  console.log('CV page')
-  // remove /cv from url
-  // const domain = window.location.href.replace(/\/cv$/, '')
-  // console.log({ domain })
-  // window.location.href = `${domain}/nmpereira_cv.pdf`
+  const domain = window.location.href.replace(/\/cv$/, '')
+  window.location.href = `${domain}/nmpereira_cv.pdf`
 
-  return <h1>CV</h1>
+  return null
 }
 
 export default Cv

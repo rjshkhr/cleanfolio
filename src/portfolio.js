7067fad8f9f62c2e59cd17813d34595353054cc4
chantelcarter <chantel.alexia@gmail.com>
2024-03-10 13:32:34 -0700

    small changes

--- src/portfolio.js
+++ src/portfolio.js
@@ -1,6 +1,6 @@
 const header = {
   // all the properties are optional - can be left empty or deleted
-  homepage: 'https://rajshekhar26.github.io/cleanfolio',
+  homepage: 'https://chantelcarter.github.io',
   title: 'JS.',
 }
 

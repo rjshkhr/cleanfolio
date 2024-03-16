0c775e5b3242ab17a90acf10564d321126cd5f73
josephmasson26 <c.p.joseph18@gmail.com>
2024-03-16 10:09:29 -0400

    basic portfolio filled out

--- src/portfolio.js
+++ src/portfolio.js
@@ -1,19 +1,19 @@
 const header = {
   // all the properties are optional - can be left empty or deleted
   homepage: 'https://rajshekhar26.github.io/cleanfolio',
-  title: 'JS.',
+  title: 'JM.',
 }
 
 const about = {
   // all the properties are optional - can be left empty or deleted
-  name: 'John Smith',
-  role: 'Front End Engineer',
+  name: 'Joseph Masson',
+  role: 'Computer Science Student',
   description:
-    'Adipisicing sit fugit ullam unde aliquid sequi Facilis soluta facilis perspiciatis corporis nulla aspernatur. Autem eligendi rerum delectus modi quisquam? Illo ut quasi nemo ipsa cumque perspiciatis! Maiores minima consectetur.',
+    'I am a student at Georgia Institute of Technology, studying Information Internetworks and Systems Architecture. I am a Teaching Assistant for Databases and Discrete Math, and develop for clubs like GreyHat and iOS Club. In my free time, I do research on Cyber-Physical Systems with robotics, and frequently compete in hackathons. I am passionate about software development and cybersecurity, and I am always looking for new opportunities to learn and grow.',
   resume: 'https://example.com',
   social: {
-    linkedin: 'https://linkedin.com',
-    github: 'https://github.com',
+    linkedin: 'https://linkedin.com/jhmusm21',
+    github: 'https://github.com/josephmasson26',
   },
 }
 
@@ -21,51 +21,62 @@ const projects = [
   // projects can be added an removed
   // if there are no projects, Projects section won't show up
   {
-    name: 'Project 1',
+    name: 'ScribbleTex',
     description:
-      'Amet asperiores et impedit aliquam consectetur? Voluptates sed a nulla ipsa officia et esse aliquam',
-    stack: ['SASS', 'TypeScript', 'React'],
-    sourceCode: 'https://github.com',
-    livePreview: 'https://github.com',
+    'AI ATL Best Use of GitHub Winner: A handwriting to LaTeX conversion tool.',
+    stack: ['React', 'Flask', 'Google Cloud', 'ML', 'Vertex-AI'],
+    sourceCode: 'https://github.com/josephmasson26/ScribbleTex',
+    livePreview: 'https://devpost.com/software/scribbletex',
   },
   {
-    name: 'Project 2',
+    name: 'DiagnoseMe',
     description:
-      'Amet asperiores et impedit aliquam consectetur? Voluptates sed a nulla ipsa officia et esse aliquam',
-    stack: ['SASS', 'TypeScript', 'React'],
-    sourceCode: 'https://github.com',
-    livePreview: 'https://github.com',
+      'Interactive chatbot application designed to simulate patient interactions for medical students.',
+    stack: ['JavaScript', 'Python', 'Electron', 'OpenAI'],
+    sourceCode: 'https://github.com/josephmasson26/DiagnoseMe',
+    livePreview: 'https://devfolio.co/projects/diagnoseme-1992',
   },
   {
-    name: 'Project 3',
+    name: 'MorseTorch',
     description:
-      'Amet asperiores et impedit aliquam consectetur? Voluptates sed a nulla ipsa officia et esse aliquam',
-    stack: ['SASS', 'TypeScript', 'React'],
-    sourceCode: 'https://github.com',
-    livePreview: 'https://github.com',
+      'Computer vision iOS application that translates between Morse code and English.',
+    stack: ['Swift', 'C++'],
+    sourceCode: 'https://github.com/josephmasson26/MorseTorch',
+    livePreview: 'https://devpost.com/software/morse-torch',
   },
 ]
 
 const skills = [
   // skills can be added or removed
   // if there are no skills, Skills section won't show up
+  'Java',
+  'SQL',
+  'JavaScript',
+  'Python',
+  'Swift',
+  'C',
+  'C++',
+  'Assembly',
+  'Rust',
   'HTML',
   'CSS',
-  'JavaScript',
-  'TypeScript',
+  'R',
   'React',
-  'Redux',
-  'SASS',
-  'Material UI',
+  'Electron',
+  'MySQL',
+  'Android Studio',
+  'Xcode',
   'Git',
-  'CI/CD',
-  'Jest',
-  'Enzyme',
+  'AWS',
+  'Google Cloud',
+  'Jupyter Notebook',
+  'Docker',
+  'ROS2',
 ]
 
 const contact = {
   // email is optional - if left empty Contact section won't show up
-  email: 'johnsmith@mail.com',
+  email: 'joseph_masson@icloud.com',
 }
 
 export { header, about, projects, skills, contact }

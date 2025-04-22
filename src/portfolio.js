d41c95b2affb062d9b561e5883948238f638503c
Leon Lam <leonlam34@gmail.com>
2025-04-22 12:21:27 -0500

    adjusted site for me

--- src/portfolio.js
+++ src/portfolio.js
@@ -1,25 +1,21 @@
 const header = {
-  // all the properties are optional - can be left empty or deleted
-  homepage: 'https://rjshkhr.github.io/cleanfolio',
-  title: 'JS.',
+  homepage: 'https://leonlam.xyz',
+  title: 'LL.',
 }
 
 const about = {
-  // all the properties are optional - can be left empty or deleted
-  name: 'John Smith',
-  role: 'Front End Engineer',
+  name: 'Leon Lam',
+  role: 'Back End Engineer',
   description:
     'Adipisicing sit fugit ullam unde aliquid sequi Facilis soluta facilis perspiciatis corporis nulla aspernatur. Autem eligendi rerum delectus modi quisquam? Illo ut quasi nemo ipsa cumque perspiciatis! Maiores minima consectetur.',
   resume: 'https://example.com',
   social: {
-    linkedin: 'https://linkedin.com',
-    github: 'https://github.com',
+    linkedin: 'https://linkedin.com/in/leon-m-lam/',
+    github: 'https://github.com/Leon-Lam919',
   },
 }
 
 const projects = [
-  // projects can be added an removed
-  // if there are no projects, Projects section won't show up
   {
     name: 'Project 1',
     description:
@@ -49,23 +45,23 @@ const projects = [
 const skills = [
   // skills can be added or removed
   // if there are no skills, Skills section won't show up
+  'C',
+  'C++',
+  'Java',
+  'Python',
   'HTML',
   'CSS',
   'JavaScript',
-  'TypeScript',
   'React',
-  'Redux',
   'SASS',
   'Material UI',
   'Git',
   'CI/CD',
-  'Jest',
-  'Enzyme',
 ]
 
 const contact = {
   // email is optional - if left empty Contact section won't show up
-  email: 'johnsmith@mail.com',
+  email: 'leonlam34@gmail.com',
 }
 
 export { header, about, projects, skills, contact }

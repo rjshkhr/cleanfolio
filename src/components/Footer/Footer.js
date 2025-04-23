b1467618fb5284de09afc31bd273a62552173a37
Leon Lam <leonlam34@gmail.com>
2025-04-22 21:36:08 -0500

    updated footer

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -2,11 +2,8 @@ import './Footer.css'
 
 const Footer = () => (
   <footer className='footer'>
-    <a
-      href='https://github.com/rjshkhr/cleanfolio'
-      className='link footer__link'
-    >
-      Created By Raj Shekhar
+    <a>
+      Made with love By Leon Lam
     </a>
   </footer>
 )

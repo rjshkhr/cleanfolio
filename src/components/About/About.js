a266b4da818cfe192e91cd83f089c94a5a5b649b
DHONDGE Hrishikesh <hrishikesh.dhondge@loria.fr>
2025-08-17 13:26:20 +0530

    Feature: Support for profile picture (optional)

--- src/components/About/About.js
+++ src/components/About/About.js
@@ -4,10 +4,24 @@ import { about } from '../../portfolio'
 import './About.css'
 
 const About = () => {
-  const { name, role, description, resume, social } = about
+  const { name, role, description, resume, social, picture } = about
 
   return (
     <div className='about center'>
+      <div className='about__header'>
+        {picture && (
+          <img
+            src={
+              picture.startsWith('http')
+                ? picture
+                : `${process.env.PUBLIC_URL}/images/${picture}`
+            }
+            alt={name}
+            className='about__picture'
+          />
+        )}
+
+    <div className='about__intro'>
       {name && (
         <h1>
           Hi, I am <span className='about__name'>{name}.</span>
@@ -16,6 +30,8 @@ const About = () => {
 
       {role && <h2 className='about__role'>A {role}.</h2>}
       <p className='about__desc'>{description && description}</p>
+      </div>
+      </div>
 
       <div className='about__contact center'>
         {resume && (
--- src/portfolio.js
+++ src/portfolio.js
@@ -8,6 +8,8 @@ const about = {
   // all the properties are optional - can be left empty or deleted
   name: 'John Smith',
   role: 'Front End Engineer',
+  picture: 'https://github.githubassets.com/assets/GitHub-Mark-ea2971cee799.png',
+
   description:
     'Adipisicing sit fugit ullam unde aliquid sequi Facilis soluta facilis perspiciatis corporis nulla aspernatur. Autem eligendi rerum delectus modi quisquam? Illo ut quasi nemo ipsa cumque perspiciatis! Maiores minima consectetur.',
   resume: 'https://example.com',
